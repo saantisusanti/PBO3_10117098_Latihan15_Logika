9aea227b8d0ccad566bf6bea716c3adced37041c
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:26:20 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan15_Logika/src/pbo3_10117098_latihan15_logika/PBO3_10117098_Latihan15_Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan15_logika;
+
+/**
+ * 
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
